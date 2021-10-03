f368f73c4da62e80a2ed709d34c3e415db336e84
Bilal Alptekin <bilalalptekin362@gmail.com>
2021-10-03 20:48:13 +0300

    V:1.0.1

---
+++ pcShutDowner/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace pcShutDowner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.btnUyku = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblKalansure = new System.Windows.Forms.Label();
+            this.cmbSaat = new System.Windows.Forms.ComboBox();
+            this.cmbDakika = new System.Windows.Forms.ComboBox();
+            this.cmbSaniye = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKapat.Location = new System.Drawing.Point(115, 368);
+            this.btnKapat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(264, 70);
+            this.btnKapat.TabIndex = 0;
+            this.btnKapat.Text = "Kapatma";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // btnUyku
+            // 
+            this.btnUyku.Enabled = false;
+            this.btnUyku.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUyku.Location = new System.Drawing.Point(429, 368);
+            this.btnUyku.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUyku.Name = "btnUyku";
+            this.btnUyku.Size = new System.Drawing.Size(264, 70);
+            this.btnUyku.TabIndex = 1;
+            this.btnUyku.Text = "Uyku";
+            this.btnUyku.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(223, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kalan Süre:";
+            // 
+            // lblKalansure
+            // 
+            this.lblKalansure.AutoSize = true;
+            this.lblKalansure.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKalansure.Location = new System.Drawing.Point(385, 39);
+            this.lblKalansure.Name = "lblKalansure";
+            this.lblKalansure.Size = new System.Drawing.Size(185, 29);
+            this.lblKalansure.TabIndex = 3;
+            this.lblKalansure.Text = "0 sa 0 dk 0 sn";
+            // 
+            // cmbSaat
+            // 
+            this.cmbSaat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbSaat.FormattingEnabled = true;
+            this.cmbSaat.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23"});
+            this.cmbSaat.Location = new System.Drawing.Point(155, 212);
+            this.cmbSaat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbSaat.Name = "cmbSaat";
+            this.cmbSaat.Size = new System.Drawing.Size(121, 37);
+            this.cmbSaat.TabIndex = 5;
+            // 
+            // cmbDakika
+            // 
+            this.cmbDakika.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbDakika.FormattingEnabled = true;
+            this.cmbDakika.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.cmbDakika.Location = new System.Drawing.Point(329, 212);
+            this.cmbDakika.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbDakika.Name = "cmbDakika";
+            this.cmbDakika.Size = new System.Drawing.Size(121, 37);
+            this.cmbDakika.TabIndex = 6;
+            // 
+            // cmbSaniye
+            // 
+            this.cmbSaniye.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbSaniye.FormattingEnabled = true;
+            this.cmbSaniye.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.cmbSaniye.Location = new System.Drawing.Point(504, 212);
+            this.cmbSaniye.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbSaniye.Name = "cmbSaniye";
+            this.cmbSaniye.Size = new System.Drawing.Size(121, 37);
+            this.cmbSaniye.TabIndex = 7;
+            this.cmbSaniye.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(281, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "sa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(456, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "dk";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(631, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "sn";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(268, 292);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 70);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "İptal Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbSaniye);
+            this.Controls.Add(this.cmbDakika);
+            this.Controls.Add(this.cmbSaat);
+            this.Controls.Add(this.lblKalansure);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUyku);
+            this.Controls.Add(this.btnKapat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.Button btnUyku;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblKalansure;
+        private System.Windows.Forms.ComboBox cmbSaat;
+        private System.Windows.Forms.ComboBox cmbSaniye;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbDakika;
+        
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ pcShutDowner/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pcShutDowner
+{
+    public partial class Form1 : Form
+    {
+       
+        private int second;
+        private int minute;
+        private int hour;
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Stop();
+            
+            cmbSaniye.Text = "0";
+            cmbSaat.Text = "0";
+            cmbDakika.Text = "5";
+
+            
+          
+            
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+            second = Convert.ToInt32(cmbSaniye.Text);
+            minute= Convert.ToInt32(cmbDakika.Text);
+            hour = Convert.ToInt32(cmbSaat.Text);
+            kapat(hour, minute, second);
+            
+
+            
+        }
+        static int kapatmasuresi(int saat,int dakika,int saniye)
+        {
+            int sure = saat * 3600 + dakika * 60 + saniye;
+            return sure;
+        }
+        static void kapat(int saat,int dakika,int saniye)
+        {
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.FileName = "CMD.exe";
+            startInfo.Arguments = $"/k shutdown -s -t {kapatmasuresi(saat, dakika, saniye)}";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process.Start(startInfo);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (second>=0 && hour>=0 && minute>=0) {
+
+                second--;
+                if (second == -1)
+                {
+                    second = 59;
+                    minute--;
+                    if (minute % 60 == -1)
+                    {
+                        minute = 59;
+                        hour--;
+                    }
+                }
+            }
+            lblKalansure.Text = $"{hour.ToString()} sa {minute.ToString()} dk {second.ToString()} sn ";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+        private void uykuyaal(int saat, int dakika, int saniye)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.FileName = "CMD.exe";
+            startInfo.Arguments = $"/k shutdown -s -t {kapatmasuresi(saat, dakika, saniye)}";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process.Start(startInfo);
+        }
+        private void iptalet()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.FileName = "CMD.exe";
+            startInfo.Arguments = $"/k shutdown -a";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process.Start(startInfo);
+            timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            iptalet();
+            cmbSaniye.Text = "0";
+            cmbSaat.Text = "0";
+            cmbDakika.Text = "5";
+            lblKalansure.Text = "0 sa 0 dk 0 sn";
+        }
+    }
+}
